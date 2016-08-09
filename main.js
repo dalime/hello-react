a46ec883364f8f26bfbf18915d44a0ca058d30aa
duhdanube <Danny.B.Lim@gmail.com>
2016-08-09 01:34:27 -0700

    Finish timer functionality

--- main.js
+++ main.js
@@ -2,53 +2,85 @@ var HelloWorld = React.createClass({
   getInitialState: function() {
     // initializes state of component
     return {
-      counter: 0,
-      counter2: 100,
-      time: 0,
-      secondsElapsed: 0
+      counter1: 0,
+      counter2: 0,
+      elapsed: 0,
+      intervalGoStop: "Stop"
     }
 
   },
-  addToCounter: function() {
-    this.setState({counter: this.state.counter + 1, counter2: this.state.counter2 + 1})
-  },
-  subFromCounter: function() {
-    if (this.state.counter2 > 0) {
-      this.setState({counter2: this.state.counter2 - 1})
-    }
-    if (this.state.counter > 0) {
-      this.setState({counter: this.state.counter - 1})
+  addToCounter1: function() {
+    if (this.state.intervalGoStop === "Stop") {
+      this.setState({counter1: this.state.counter1 + 1})
     }
   },
-  tick: function() {
-    this.setState({secondsElapsed: this.state.secondsElasped + 1});
+  subFromCounter1: function() {
+    if (this.state.intervalGoStop === "Stop") {
+      if (this.state.counter1 > 0) {
+        this.setState({counter1: this.state.counter1 - 1})
+      }
+    }
+  },
+  addToCounter2: function() {
+    if (this.state.intervalGoStop === "Stop") {
+      this.setState({counter2: this.state.counter2 + 1})
+    }
+  },
+  subFromCounter2: function() {
+    if (this.state.intervalGoStop === "Stop") {
+      if (this.state.counter2 > 0) {
+        this.setState({counter2: this.state.counter2 - 1})
+      }
+    }
   },
   componentDidMount: function() {
-    this.interval = setInterval(this.tick, 1000);
+    this.timer = window.setInterval(this.tick, 1000);
   },
   componentWillUnmount: function() {
-    clearInterval(this.interval);
+    window.clearInterval(this.timer);
+  },
+  tick: function() {
+    if (this.state.intervalGoStop === "Stop") {
+      this.setState({elapsed: this.state.elapsed + 1});
+    } else {
+      this.setState({elapsed: this.state.elapsed});
+    }
+  },
+  tock: function() {
+    this.setState({elapsed: this.state.elapsed});
+  },
+  timeState: function() {
+    if (this.state.intervalGoStop === "Stop") {
+      this.setState({intervalGoStop: "Go"});
+    } else {
+      this.setState({intervalGoStop: "Stop"});
+    }
   },
   render: function() {
     return (
       <div>
         <h1>Hello from React</h1>
         <p>Let's count stuff</p>
-        <h3>Auto Time: {this.state.secondsElapsed}</h3>
+        <h3>Auto Time: {this.state.elapsed}</h3>
+        <button onClick={this.timeState}>{this.state.intervalGoStop}</button>
         <hr/>
         <div>
-          Counter: {this.state.counter === 10 ? 'WOO TEN!!!' : this.state.counter}
+          Counter 1: {this.state.counter1}
         </div>
         <div>
-          Counter: {this.state.counter2 === 50 ? 'WOO FIFTY!!!' : this.state.counter2}
+          <button onClick={this.addToCounter1}>+</button>
+          <button onClick={this.subFromCounter1}>-</button>
         </div>
-        <button onClick={this.addToCounter}>+</button>
-        <button onClick={this.subFromCounter}>-</button>
+        <div>
+          Counter 2: {this.state.counter2}
+        </div>
+        <button onClick={this.addToCounter2}>+</button>
+        <button onClick={this.subFromCounter2}>-</button>
       </div>
     );
   }
 });
-//        <button onClick={this.startStopTimer}>GO</button>
+
   ReactDOM.render(
     <HelloWorld />,
     document.getElementById('example')
